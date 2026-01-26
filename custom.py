7f9a0c89b01028cf7b0ea1762942076e3aab3094
cl4cnam <40176886+cl4cnam@users.noreply.github.com>
2026-01-26 21:08:01 +0100

    Add 'colorThisDrawing' and 'highlightSequence' functions

--- custom.py
+++ custom.py
@@ -24,6 +24,7 @@ Description of this extension
 import inkex
 
 def custom(p_options):
+	# p_options.msg(p_options.funcsug.replace(r'\n', '\n'))
 	return r'''title
 ===+++===+++===
 /* CSS part */
@@ -41,6 +42,47 @@ function getAll(ps_matchList) {
 			return elts
 		})
 		.flat()
+}
+
+function getAllMatches(ps_matchList) {
+	const pArray_matchList = ps_matchList.split(',')
+	return {seq: pArray_matchList
+		.map(cl=>{
+			const elts = Array.from(document.querySelectorAll('.'+cl))
+			return elts
+		})}
+}
+
+function setCursorColor(color) {
+	const cursor = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32'%3E%3Cpath fill='%23fed' stroke='black' d='m 0,0 20,0 12,12 0,20 -20,0 -12,-12 z'/%3E%3Ccircle fill='` + color + `' stroke='black' cx='0' cy='0' r='20'/%3E%3Cpath stroke='black' fill='none' d='m 20,0 -20,0 0,20' /%3E%3C/svg%3E") 0 0, pointer`
+	document.documentElement.style.cursor = cursor
+}
+
+function getDot(color, position) {
+	return `<circle cx="${position*25+20}" cy="20" r="10" fill="${color}" stroke="black">${color}</circle>`
+}
+
+function getRowOfDots(colors) {
+	return '<svg id="palette" transform="translate(0 0)" width="' + colors.length*30 + '" height="40">' + colors.map((e,i)=>getDot(e, i)).join('') + '</svg>'
+}
+
+function highlight(list, direction) {
+	if (list.num === undefined) {
+		if (direction == 1) list.num = -1
+		else return
+	} else {
+		// remove old highlighting
+		const elts = list.seq[list.num]
+		elts.forEach(elt=>{elt.classList.remove('highlight')})
+	}
+	// go in direction
+	list.num += direction
+	list.num %= list.seq.length
+	list.num += list.seq.length
+	list.num %= list.seq.length
+	// add highlighting
+	const elts = list.seq[list.num]
+	elts.forEach(elt=>{elt.classList.add('highlight')})
 }
 
 function fixMessageDisplay1(selector) {
@@ -83,6 +125,52 @@ def showNewMessageWithidUntilClick(p_message, p_id, p_messageXShift, p_messageYS
 			'pause'
 		var messageSelector := '#' + p_id
 		calljs fixMessageDisplay(messageSelector)
+def colorThisDrawing(p_colors):
+	parallel ||
+		showNewElementIn(calljs getRowOfDots(p_colors), 'svg', 'g:svg', false, 'forever')
+	||
+		var color := 'white'
+		calljs setCursorColor(color)
+
+		parallel:
+			sequence:
+				var selector := '#palette'
+				calljs fixMessageDisplay(selector)
+			while true:
+				var paletteEvt := awaitDomeventBeep('mousedown', '#palette')
+				var palettePart := paletteEvt.target
+				color := palettePart.innerHTML
+				calljs setCursorColor(color)
+			while true:
+				var drawingEvt := awaitDomeventBeep('mousedown', 'svg')
+				var drawingPart := drawingEvt.target
+				drawingPart.style.fill := color
+def highlightSequence(p_sequence):
+	var elementsToFind := calljs getAllMatches(p_sequence)
+	parallel ||
+		var direction := 1
+		while true:
+			awaitClickBeep('svg')
+			calljs highlight(elementsToFind, direction)
+	||
+		var direction := 1
+		while true:
+			var evt := awaitDomeventBeep('keydown', 'svg')
+			if evt.key = 'ArrowRight':
+				calljs highlight(elementsToFind, direction)
+	||
+		var direction := -1
+		while true:
+			var evt := awaitDomeventBeep('keydown', 'svg')
+			if evt.key = 'Backspace' or evt.key = 'ArrowLeft':
+				calljs highlight(elementsToFind, direction)
+		onBreak:
+			js (elementsToFind):
+				if (elementsToFind.num !== undefined) {
+					// remove old highlighting
+					const elts = elementsToFind.seq[elementsToFind.num]
+					elts.forEach(elt=>{elt.classList.remove('highlight')})
+				}
 
 ''' + p_options.funcsug.replace(r'\n', '\n')
 
@@ -96,6 +184,7 @@ class Gamify(inkex.EffectExtension):
 	def effect(self):
 		# custom = self.svg.metadata.findone(f"rdf:RDF/cc:Work/dc:description").text
 		import gamify
+		# self.options.msg = self.msg
 		gamify.gamify(self.svg, custom(self.options), self.msg)
 
 if __name__ == '__main__':
